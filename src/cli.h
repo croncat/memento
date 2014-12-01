45a6a327549c3056d60260568978b14743bed8b7
Emiliano Canedo <emilianocanedo@gmail.com>
2014-12-01 15:05:11 -0300

    First commit!

---
+++ src/cli.h
@@ -0,0 +1,108 @@
+/* Copyright (c) 2014, Emiliano Gabriel Canedo <emilianocanedo@gmail.com>
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of memento nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CLI_H
+#define CLI_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "memento.h"
+
+namespace Cli
+{
+    void AddCategory(int argc, char *argv[])
+    {
+        if (argc == 3 && strncmp(argv[1],"-a", 2) == 0 && strlen(argv[1]) == 2) {
+
+            Memento::AddCategory(argv[2]);
+            exit(0);
+        }
+    }
+
+    void ListCategories(int argc, char *argv[])
+    {
+        if (argc == 2 && strncmp(argv[1],"-l", 2) == 0 && strlen(argv[1]) == 2) {
+
+            Memento::ListCategories();
+            exit(0);
+        }
+    }
+
+    void DeleteCategory(int argc, char *argv[])
+    {
+        if (argc == 3 && strncmp(argv[1],"-d", 2) == 0 && strlen(argv[1]) == 2) {
+
+            Memento::DeleteCategory(argv[2]);
+            exit(0);
+        }
+    }
+
+    void NewMessage(int argc, char *argv[])
+    {
+        if (argc == 4 && strncmp(argv[1],"-n", 2) == 0 && strlen(argv[1]) == 2) {
+
+            Memento::NewMessage(argv[2], argv[3]);
+            exit(0);
+        }
+    }
+
+    void ShowMessages(int argc, char *argv[])
+    {
+        if (argc == 3 && strncmp(argv[1],"-s", 2) == 0 && strlen(argv[1]) == 2) {
+
+            Memento::ShowMessages(argv[2]);
+            exit(0);
+        }
+    }
+
+    void HelpFeature()
+    {
+        printf("memento 0.1a [alpha] ( http://github.com/memento )\n");
+        printf("Usage: memento [OPTION] {ARGUMENT/S}\n");
+        printf("OPTIONS:\n");
+        printf("  -l: list all categories\n");
+        printf("  -a: add category\n");
+        printf("  -d: delete category\n");
+        printf("  -n: add new message to category\n");
+        printf("  -s: show all messages in category\n");
+        printf("EXAMPLES:\n");
+        printf("  memento -l\n");
+        printf("  memento -a quotes\n");
+        printf("  memento -d commands\n");
+        printf("  memento -n quotes \"my first quote, yeah!\"\n");
+        printf("  memento -s films\n");
+        printf("memento: minimalist organizer\n");
+
+        exit(0);
+    }
+}
+
+#endif
---
+++ src/memento.h
@@ -0,0 +1,135 @@
+/* Copyright (c) 2014, Emiliano Gabriel Canedo <emilianocanedo@gmail.com>
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of memento nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef MEMENTO_H
+#define MEMENTO_H
+
+#include <stdlib.h>
+#include <sqlite3.h>
+
+namespace Memento
+{
+    sqlite3 *db;
+
+    static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+    {
+        int i;
+        for(i=0; i<argc; i++) {
+            printf("- %s\n", argv[i] ? argv[i] : "NULL");
+        }
+        //printf("\n");
+        return 0;
+    }
+
+    void OpenMementoDatabase()
+    {
+        const char* home;
+        char path[256];
+        int rc;
+        
+        home = getenv("HOME");
+        sprintf(path, "%s/.memento.db", home);
+        
+        rc = sqlite3_open(path, &db);
+        if(rc) {
+            fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+            sqlite3_close(db);
+        }
+    }
+
+    void ExecMementoDatabase(char *query)
+    {
+        int rc;
+        char *zErrMsg = 0;
+
+        rc = sqlite3_exec(db, query, callback, 0, &zErrMsg);
+        if( rc!=SQLITE_OK ){
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }
+    } 
+
+    void ListCategories()
+    {
+        char query[256];
+
+        sprintf(query, "SELECT name FROM sqlite_master WHERE type = \"table\" AND name != \"sqlite_sequence\"");
+
+        OpenMementoDatabase();
+        ExecMementoDatabase(query);
+        sqlite3_close(db);
+    }
+
+    void AddCategory(const char *category)
+    {
+        char query[256];
+
+        sprintf(query, "CREATE TABLE %s(ID INTEGER PRIMARY KEY AUTOINCREMENT, MESSAGE TEXT NOT NULL)\0", category);
+
+        OpenMementoDatabase();
+        ExecMementoDatabase(query);
+        sqlite3_close(db);
+    }
+
+    void DeleteCategory(const char *category)
+    {
+        char query[256];
+
+        sprintf(query, "DROP TABLE %s\0", category);
+
+        OpenMementoDatabase();
+        ExecMementoDatabase(query);
+        sqlite3_close(db);
+    }
+
+    void NewMessage(const char *category, const char *message)
+    {
+        char query[2048];
+
+        sprintf(query, "INSERT INTO %s (MESSAGE) VALUES ('%s')\0", category, message);
+
+        OpenMementoDatabase();
+        ExecMementoDatabase(query);
+        sqlite3_close(db);
+    }
+
+    void ShowMessages(const char *category)
+    {
+        char query[256];
+
+        sprintf(query, "SELECT MESSAGE FROM %s\0", category);
+
+        OpenMementoDatabase();
+        ExecMementoDatabase(query);
+        sqlite3_close(db);
+    }
+}
+
+#endif
